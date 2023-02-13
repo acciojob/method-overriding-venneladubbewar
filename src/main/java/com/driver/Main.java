275814c7597bfb7d6c69c7ef38ea3c80dc66b26d
kiran <Kiran.dubbewar18@users.noreply.github.com>
2023-02-13 21:55:59 +0530

    first

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -18,8 +18,11 @@ public class Main {
 
     public static void main(String args[]) {
       B b = new B();
-        System.out.println(b.meth());
-        System.out.println(b.meth());
+      b.meth();
+       // System.out.println(b.meth());
+      //  System.out.println(b.meth());
+        B b1 = new B();
+        b.meth();
 
 
     }
